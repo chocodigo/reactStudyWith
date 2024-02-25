648b24a9e443f4246253426f050ce394b572e3ad
chocodigo <chocodigo>
2024-02-25 22:19:47 +0900

    Home 내용 정리

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,27 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import axios from "axios";
+import { NotionRenderer } from "react-notion";
 
 const Home = () => {
-  return <Stack>Home</Stack>;
+  const [response, setResponse] = useState();
+  const NOTION_PAGE_ID = "Study-3dde10f909704223980a9e67fb551090";
+
+  useEffect(() => {
+    axios
+      .get(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
+      .then(({ data }) => {
+        setResponse(data);
+      });
+  });
+
+  return response ? (
+    <NotionRenderer blockMap={response} fullPage={true} />
+  ) : (
+    <></>
+  );
 };
 
 export default Home;
